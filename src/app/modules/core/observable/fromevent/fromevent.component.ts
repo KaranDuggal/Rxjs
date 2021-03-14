3492f128bc929de20d6026050b54888112ce06fa
Karan <duggalkaran400@gmail.com>
2021-03-14 10:49:23 +0530

    form event

--- src/app/modules/core/observable/fromevent/fromevent.component.ts
+++ src/app/modules/core/observable/fromevent/fromevent.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {fromEvent} from 'rxjs'
 
 @Component({
   selector: 'app-fromevent',
   templateUrl: './fromevent.component.html',
   styleUrls: ['./fromevent.component.scss']
 })
-export class FromeventComponent implements OnInit {
-
+export class FromeventComponent implements OnInit, AfterViewInit {
+  @ViewChild('addBtn') addBtn:ElementRef|undefined;
+  count = 1
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
+  ngAfterViewInit(){
+    fromEvent(this.addBtn?.nativeElement,'click').subscribe(res=>{
+      this.print('click')
+    })
   }
-
+  print(event:any){
+    let el = document.createElement('li')
+    el.innerText = `event ${this.count++}`
+    document.getElementById('elContainer')?.appendChild(el)
+  }
 }
