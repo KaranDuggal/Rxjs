738b0754eabe5771cb9b3ae2b65e717abda1bc5b
Karan <duggalkaran400@gmail.com>
2021-03-14 14:34:38 +0530

    interval

--- src/app/modules/core/observable/interval/interval.component.ts
+++ src/app/modules/core/observable/interval/interval.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription, timer } from 'rxjs';
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service';
 
 @Component({
   selector: 'app-interval',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interval.component.scss']
 })
 export class IntervalComponent implements OnInit {
-
-  constructor() { }
+  obsMsg:any
+  videoSubscription:Subscription|undefined
+  constructor(
+    private designingutilityServices:DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    // const broadCastVideos = interval(2000)
+    // timer(delay,interval)
+    const broadCastVideos = timer(5000,1000)
+
+    this.videoSubscription =  broadCastVideos.subscribe(res=>{
+      this.obsMsg = 'Video '+res
+      this.designingutilityServices.print(this.obsMsg,"stream")
+      
+      if(res >= 5){
+        this.videoSubscription?.unsubscribe()
+      }
+      
+    })
   }
 
 }
