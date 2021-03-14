294c8d1cb073c620633b46a6bbf85b68c7259ea4
Karan <duggalkaran400@gmail.com>
2021-03-14 11:15:50 +0530

    interval

--- src/app/modules/core/observable/observable-routing.module.ts
+++ src/app/modules/core/observable/observable-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FromeventComponent } from './fromevent/fromevent.component';
+import { IntervalComponent } from './interval/interval.component';
 import { ObservablelistComponent } from './observablelist/observablelist.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path:'fromevent',
     component:FromeventComponent
+  },
+  {
+    path:'interval',
+    component:IntervalComponent
   }
 ];
 
--- src/app/modules/core/observable/observable.module.ts
+++ src/app/modules/core/observable/observable.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ObservableRoutingModule } from './observable-routing.module';
 import { ObservablelistComponent } from './observablelist/observablelist.component';
 import { FromeventComponent } from './fromevent/fromevent.component';
-import {Router} from '@angular/router'
+import {Router} from '@angular/router';
+import { IntervalComponent } from './interval/interval.component'
 
 @NgModule({
-  declarations: [ObservablelistComponent, FromeventComponent],
+  declarations: [ObservablelistComponent, FromeventComponent, IntervalComponent],
   imports: [
     CommonModule,
     ObservableRoutingModule,
