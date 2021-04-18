09813ed3ce9c8ab159158e4b89ea85218acf4b4b
KaranDuggal <duggalkaran400@gmail.com>
2021-04-18 21:59:34 +0530

    array

--- src/app/modules/core/observable/to-array/to-array.component.ts
+++ src/app/modules/core/observable/to-array/to-array.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-to-array',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./to-array.component.scss']
 })
 export class ToArrayComponent implements OnInit {
-
   constructor() { }
-
+  sourceSub:Subscription|undefined;
+  
   ngOnInit(): void {
+    const source = interval(1000);
+    this.sourceSub = source.pipe().subscribe((res)=>{
+      console.log('res', res);
+      // if(res >= 8){
+      //   this.sourceSub?.unsubscribe()
+      // }
+    })
   }
 
 }
