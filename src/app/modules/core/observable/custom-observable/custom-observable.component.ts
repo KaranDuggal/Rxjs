054a5e25766b151c9c52d9342fa7d7aec30c230c
KaranDuggal <duggalkaran400@gmail.com>
2022-10-12 09:27:12 +0530

    custom observer manual

--- src/app/modules/core/observable/custom-observable/custom-observable.component.ts
+++ src/app/modules/core/observable/custom-observable/custom-observable.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service';
 
 @Component({
   selector: 'app-custom-observable',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custom-observable.component.scss']
 })
 export class CustomObservableComponent implements OnInit {
-
-  constructor() { }
+  customObserverManualStatus:'Error'|'Complete'|undefined
+  constructor(
+    private designUtilityService: DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    const cusObs1 = Observable.create((observer:any)=>{
+      setTimeout(() => {
+        observer.next('Data emit 1')
+      }, 1000);
+      setTimeout(() => {
+        observer.next('Data emit 2')
+        // observer.complete()
+      }, 2000);
+      setTimeout(() => {
+        observer.next('Data emit 3')
+        observer.error('Limit cross')
+      }, 3000);
+      setTimeout(() => {
+        observer.next('Data emit 4')
+      }, 4000);
+    })
+    cusObs1.subscribe((res:any)=>{
+      this.designUtilityService.print(res,'customObserverManual')
+    },()=>{
+      this.customObserverManualStatus = 'Error'
+    },()=>{
+      this.customObserverManualStatus = 'Complete'
+    })
   }
 
 }
