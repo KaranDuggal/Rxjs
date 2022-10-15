c8120ba6611d25379105c52e193449dfbe429615
KaranDuggal <duggalkaran400@gmail.com>
2022-10-15 09:34:53 +0530

    tap

--- src/app/modules/core/observable/tap/tap.component.ts
+++ src/app/modules/core/observable/tap/tap.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service';
 
 @Component({
   selector: 'app-tap',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tap.component.scss']
 })
 export class TapComponent implements OnInit {
-
-  constructor() { }
+  obsSubscription:Subscription|undefined
+  myColor:string = ''
+  constructor(
+    private designUtilityService:DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    const arr= ['Karan','Duggal','Linux','Windows','Mac',]
+
+    this.obsSubscription = interval(1000).pipe(
+      tap(data =>{
+        // if(data === 4) this.obsSubscription?.unsubscribe()
+      }),
+      map(data=>{
+        // if(data === 4) this.obsSubscription?.unsubscribe()
+        return arr[data]
+      }),
+      take(arr.length)
+    ).subscribe(res=>{
+      this.designUtilityService.print(res,'tap')
+    })
+
+    const colorArr = ['aqua' ,'red' ,'gray' ,'green' ,'greenyellow' ,'palevioletred' ,'blue' ,'blueviolet' ,'burlywood']
+    this.obsSubscription = interval(1000).pipe(
+      tap(data =>{
+        this.myColor = colorArr[data]
+        // if(data === colorArr.length - 1) this.obsSubscription?.unsubscribe()
+      }),
+      map(data=>colorArr[data]),
+      take(colorArr.length)
+    ).subscribe(res=>{
+      this.designUtilityService.print(res,'tapColor')
+    })
+    
   }
 
 }
