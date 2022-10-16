84b296500ba9320530d1c2ce65d7989216ed03f5
KaranDuggal <duggalkaran400@gmail.com>
2022-10-16 15:54:31 +0530

    take

--- src/app/modules/core/observable/take/take.component.ts
+++ src/app/modules/core/observable/take/take.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, interval, timer } from 'rxjs';
+import { take, takeLast, takeUntil } from 'rxjs/operators';
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service';
 
 @Component({
   selector: 'app-take',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TakeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private designUtilityService:DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    interval(1000).pipe(
+      take(5)
+    ).subscribe((res)=>{
+      this.designUtilityService.print(res,'take')
+
+    })
+    interval(1000).pipe(
+      take(10),
+      takeLast(5)
+    ).subscribe((res)=>{
+      this.designUtilityService.print(res,'takeLast')
+    })
+    let condition_1 = timer(5000)
+    let condition_2 = fromEvent(document,'click')
+    interval(1000).pipe(
+      // take(10),
+      takeUntil(condition_2)
+    ).subscribe((res)=>{
+      this.designUtilityService.print(res,'takeUntil')
+    })
   }
 
 }
