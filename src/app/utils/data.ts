ac6dd2bc5410bf6e22471525067c0de5251afbf1
KaranDuggal <duggalkaran400@gmail.com>
2022-10-24 09:50:07 +0530

    data add

---
+++ src/app/utils/data.ts
@@ -0,0 +1,96 @@
+export const arrOfString = [
+  'Karan',
+  'Noelle Mccarthy',
+  'Jaida Bass',
+  'Kaleigh Woodward',
+  'Lilliana Kaiser',
+  'Sandra Krause',
+  'Susan Ellison',
+  'Branden Graham',
+  'Jane Torres',
+  'Jamarion Lutz',
+]
+
+export const arrOfObject = [
+    { 
+      id:1,
+      name:'Noelle Mccarthy',
+      age:21,
+      address:{
+        pin:100501,
+        city:'Jalandhar'
+      }
+    },
+    { 
+      id:2,
+      name:'Jaida Bass',
+      age:25,
+      address:{
+        pin:100502,
+        city:'Ludhiana'
+      }
+    },
+    { 
+      id:3,
+      name:'Kaleigh Woodward',
+      age:34,
+      address:{
+        pin:100503,
+        city:'Amritsar'
+      }
+    },
+    { 
+      id:4,
+      name:'Lilliana Kaiser',
+      age:29,
+      address:{
+        pin:100501,
+        city:'Patiala'
+      }
+    },
+    { 
+      id:5,
+      name:'Sandra Krause',
+      age:14,
+      address:{
+        pin:100505,
+        city:'Bathinda'
+      }
+    },
+    { 
+      id:6,
+      name:'Susan Ellison',
+      age:21,
+      address:{
+        pin:100506,
+        city:'Hoshiarpur'
+      }
+    },
+    { 
+      id:7,
+      name:'Branden Graham',
+      age:42,
+      address:{
+        pin:100507,
+        city:'Moga'
+      }
+    },
+    { 
+      id:8,
+      name:'Jane Torres',
+      age:52,
+      address:{
+        pin:100508,
+        city:'Abohar'
+      }
+    },
+    { 
+      id:9,
+      name:'Jamarion Lutz',
+      age:47,
+      address:{
+        pin:100509,
+        city:'Khanna'
+      }
+    },
+  ]
