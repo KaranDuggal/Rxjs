7976ed307918c9e188505e52e3a1e9356c4411a3
KaranDuggal <duggalkaran400@gmail.com>
2022-10-24 13:33:38 +0530

    subject

--- src/app/modules/common/nav/nav.component.ts
+++ src/app/modules/common/nav/nav.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service'
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  isExclusive:boolean = false
+  constructor(
+    private designUtilityService:DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    this.designUtilityService.exclusive.subscribe(res=>{
+      this.isExclusive = res
+    })
   }
 
 }
--- src/app/modules/core/observable/subject/subject.component.ts
+++ src/app/modules/core/observable/subject/subject.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service'
 
 @Component({
   selector: 'app-subject',
   templateUrl: './subject.component.html',
   styleUrls: ['./subject.component.scss']
 })
-export class SubjectComponent implements OnInit {
+export class SubjectComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    private designUtilityService:DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    this.designUtilityService.exclusive.next(true)
   }
-
+ ngOnDestroy(): void {
+  this.designUtilityService.exclusive.next(false)
+ }
 }
