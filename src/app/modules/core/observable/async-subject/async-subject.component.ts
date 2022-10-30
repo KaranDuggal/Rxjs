0f2aa2dd66299688aaa58e02d0ca9409f6cea0cb
KaranDuggal <duggalkaran400@gmail.com>
2022-10-30 09:27:20 +0530

    async subject

--- src/app/modules/core/observable/async-subject/async-subject.component.ts
+++ src/app/modules/core/observable/async-subject/async-subject.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service';
 
 @Component({
   selector: 'app-async-subject',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./async-subject.component.scss']
 })
 export class AsyncSubjectComponent implements OnInit {
+  addVideo = new FormControl()
+  asyncVideoEmit: string = ''
+  asyncVideoEmitSub: Subscription|undefined
+  value:string = ''
 
-  constructor() { }
+  constructor(
+    private designUtilityService : DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    this.asyncVideoEmitSub = this.designUtilityService.asyncVideoEmit.subscribe(res=>{
+      this.asyncVideoEmit = res
+    })
   }
-
+  addVideoFun(){
+    this.designUtilityService.asyncVideoEmit.next(this.addVideo.value)
+    this.addVideo.reset()
+  }
+  onCompleteSub(){
+    this.designUtilityService.asyncVideoEmit.complete()
+  }
 }
--- src/app/services/design-utility/design-utility.service.ts
+++ src/app/services/design-utility/design-utility.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,7 @@ export class DesignUtilityService {
   // exclusive = new Subject<boolean>()
   exclusive = new BehaviorSubject<boolean>(false)
   videoEmit = new ReplaySubject<string>(5,5000)
+  asyncVideoEmit = new AsyncSubject<string>()
   print(data:any,elementId:any){
     let el = document.createElement('li')
     el.innerText = data
