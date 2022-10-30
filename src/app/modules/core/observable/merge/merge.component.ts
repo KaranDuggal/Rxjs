3f7523593067009a0ffdb2a558152a19479985e3
KaranDuggal <duggalkaran400@gmail.com>
2022-10-30 12:28:14 +0530

    Merge

--- src/app/modules/core/observable/merge/merge.component.ts
+++ src/app/modules/core/observable/merge/merge.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { concat, interval, merge } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service';
 
 @Component({
   selector: 'app-merge',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MergeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private designUtilityService : DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    const sourceTechChannel = interval(1000).pipe(
+      take(5),
+      map(v=>`techChannel video No ${v+1}`)
+    )
+    sourceTechChannel.subscribe(res=>{
+      this.designUtilityService.print(res,'techChannel')
+    })
+    const sourceDragonBallZChannel = interval(1050).pipe(
+      take(3),
+      map(v=>`dragonBallZChannel video No ${v+1}`)
+    )
+    sourceDragonBallZChannel.subscribe(res=>{
+      this.designUtilityService.print(res,'dragonBallZChannel')
+    })
+    const sourceLinuxChannel = interval(1150).pipe(
+      take(4),
+      map(v=>`linuxChannel video No ${v+1}`)
+    )
+    sourceLinuxChannel.subscribe(res=>{
+      this.designUtilityService.print(res,'linuxChannel')
+    })
+    
+    merge(sourceTechChannel,  
+      sourceDragonBallZChannel,  
+      sourceLinuxChannel).subscribe(res=>{
+      this.designUtilityService.print(res,'finalData')
+    })
   }
 
 }
