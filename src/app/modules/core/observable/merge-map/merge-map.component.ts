9238f70d5a15f27f2f484d6dc0ec0d909fc2f13a
KaranDuggal <duggalkaran400@gmail.com>
2022-11-06 07:47:17 +0530

    merge map

--- src/app/modules/core/observable/merge-map/merge-map.component.ts
+++ src/app/modules/core/observable/merge-map/merge-map.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { map, mergeAll, mergeMap } from 'rxjs/operators';
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service';
+ import { arrOfString } from "src/app/utils/data";
 
 @Component({
   selector: 'app-merge-map',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MergeMapComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private designUtilityService : DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    // Ex-1
+    const source1 = from(arrOfString).pipe(
+      map(res=> this.getData(res))
+    ).subscribe(res=>res.subscribe(res=>{
+      this.designUtilityService.print(res,'map')
+    }))
+    // Ex-2 | merge + mergeAll
+    const source2 = from(arrOfString).pipe(
+      map(res=> this.getData(res)),
+      mergeAll()
+    ).subscribe(res=>{
+      this.designUtilityService.print(res,'mapPlusMergeAll')
+    })
+    // Ex-2 | mapMerge
+    const source3 = from(arrOfString).pipe(
+      mergeMap(res=> this.getData(res))
+    ).subscribe(res=>{
+      this.designUtilityService.print(res,'mergeAll')
+    })
   }
-
+  getData(data:string){
+    return of(data+' video uploaded')
+  }
 }
