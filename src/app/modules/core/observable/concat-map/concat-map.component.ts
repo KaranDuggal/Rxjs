447d98e4184cc196cd1077cff4c23c1d3802154a
KaranDuggal <duggalkaran400@gmail.com>
2022-11-06 08:12:55 +0530

    concat map

--- src/app/modules/core/observable/concat-map/concat-map.component.ts
+++ src/app/modules/core/observable/concat-map/concat-map.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { concatAll, concatMap, delay, map, mergeMap } from 'rxjs/operators';
+import { DesignUtilityService } from 'src/app/services/design-utility/design-utility.service';
+import { arrOfString } from 'src/app/utils/data';
 
 @Component({
   selector: 'app-concat-map',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConcatMapComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private designUtilityService : DesignUtilityService
+  ) { }
 
   ngOnInit(): void {
+    // Ex-1
+    from(arrOfString).pipe(
+      map(res=> this.getData(res))
+    ).subscribe(res=>res.subscribe(res=>{
+      this.designUtilityService.print(res,'map')
+    }))
+    // Ex-2 | concat + concatAll
+    from(arrOfString).pipe(
+      map(res=> this.getData(res)),
+      concatAll()
+    ).subscribe(res=>{
+      this.designUtilityService.print(res,'mapPlusConcatAll')
+    })
+    // Ex-2 | concatAll
+    from(arrOfString).pipe(
+      concatMap(res=> this.getData(res))
+      // mergeMap(res=> this.getData(res))
+    ).subscribe(res=>{
+      this.designUtilityService.print(res,'concatAll')
+    })
   }
-
+  getData(data:string){
+    return of(data+' video uploaded').pipe(delay(1000))
+  }
 }
