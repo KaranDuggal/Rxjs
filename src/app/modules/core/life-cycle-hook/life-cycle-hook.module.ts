0b9ab1cd5182d800a58a0c8fca185c3ce097fcce
KaranDuggal <duggalkaran400@gmail.com>
2023-05-24 20:19:32 +0530

    child comp add

--- src/app/modules/core/life-cycle-hook/life-cycle-hook.module.ts
+++ src/app/modules/core/life-cycle-hook/life-cycle-hook.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LifeCycleHookRoutingModule } from './life-cycle-hook-routing.module';
 import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 
 @NgModule({
-  declarations: [ParentComponent],
+  declarations: [ParentComponent, ChildComponent],
   imports: [
     CommonModule,
     LifeCycleHookRoutingModule
--- src/app/modules/core/life-cycle-hook/parent/parent.component.ts
+++ src/app/modules/core/life-cycle-hook/parent/parent.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss']
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements OnInit, OnChanges {
 
+  @ViewChild('text') text:string = ''
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('parent comp OnChanges');
+  }
 
   ngOnInit(): void {
+    console.log('parent comp OnInit');
+    
   }
 
 }
